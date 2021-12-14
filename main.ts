65e94b1739248688684eef019691b71821001356
Sean Clark <seanc@interactdigitalarts.uk>
2021-12-14 17:30:10 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,6 @@ input.onButtonPressed(Button.A, function () {
     if (hue > 360) {
         hue = 0
     }
-    strip.showColor(neopixel.hsl(hue, saturation, luminosity))
     radio.sendValue("hue", hue)
 })
 input.onButtonPressed(Button.B, function () {
@@ -11,7 +10,6 @@ input.onButtonPressed(Button.B, function () {
     if (luminosity > 50) {
         luminosity = 0
     }
-    strip.showColor(neopixel.hsl(hue, saturation, luminosity))
     radio.sendValue("luminosity", luminosity)
 })
 radio.onReceivedValue(function (name, value) {
@@ -24,9 +22,7 @@ radio.onReceivedValue(function (name, value) {
     if (name == "luminosity") {
         luminosity = value
     }
-    strip.showColor(neopixel.hsl(hue, saturation, luminosity))
 })
-let strip: neopixel.Strip = null
 let luminosity = 0
 let saturation = 0
 let hue = 0
@@ -34,6 +30,8 @@ radio.setGroup(1)
 hue = 0
 saturation = 99
 luminosity = 50
-strip = neopixel.create(DigitalPin.P1, 30, NeoPixelMode.RGB)
-strip.showColor(neopixel.hsl(hue, saturation, luminosity))
+let strip = neopixel.create(DigitalPin.P1, 30, NeoPixelMode.RGB)
 basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+    strip.showColor(neopixel.hsl(hue, saturation, luminosity))
+})
