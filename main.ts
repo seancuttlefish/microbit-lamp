3fecbb2fe4d8d0e175fa7a9f6ce70b9c4b023389
Sean Clark <seanc@interactdigitalarts.uk>
2021-12-14 16:48:39 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,22 @@
-let strip = neopixel.create(DigitalPin.P1, 40, NeoPixelMode.RGB)
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    hue += 30
+    if (hue > 360) {
+        hue = 0
+    }
+    strip.showColor(neopixel.hsl(hue, saturation, luminosity))
 })
+input.onButtonPressed(Button.B, function () {
+    luminosity += 10
+    if (luminosity > 50) {
+        luminosity = 0
+    }
+    strip.showColor(neopixel.hsl(hue, saturation, luminosity))
+})
+let strip: neopixel.Strip = null
+let luminosity = 0
+let saturation = 0
+let hue = 0
+hue = 0
+saturation = 99
+luminosity = 50
+strip = neopixel.create(DigitalPin.P1, 30, NeoPixelMode.RGB)
